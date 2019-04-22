6b9024c52c2127dae8f0da7f5aaa4fed7ef9d5a7
Kyra <kyrayang.5@gmail.com>
2019-04-22 19:02:21 +0800

    bar set

---
+++ pages/index/index.js
@@ -0,0 +1,18 @@
+Page({
+  data:{
+    classTitle: ['国内','社会','国际','财经','娱乐','体育','军事','凑数的','其他']
+  }
+ /* onLoad(){
+    /* API接入
+    wx.request({
+      url: 'https://route.showapi.com/109-35',
+      data:{
+        "showapi_appid": '92969', //这里需要改成自己的appid
+        "showapi_sign": 'eedcb02a3323485a84c33b72074df757',  //这里需要改成自己的应用的密钥secret
+      },
+      success: res => {
+        console.log(res.data.pagebean.contentlist)
+      }
+    })*/
+   
+})
