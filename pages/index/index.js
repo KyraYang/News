5a230034b98f16fa12bce4b0340d0fc1c0608a69
Kyra <kyrayang.5@gmail.com>
2019-04-23 00:29:53 +0800

    news list set

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,13 +1,61 @@
 Page({
   data: {
     classTitle: ['国内', '社会', '国际', '财经', '娱乐', '体育', '军事', '凑数的', '其他'],
-    latestImgUrls: [
-      'http://i2.chinanews.com/simg/2019/190422//98257910.JPG', '../../images/test2.jpg', '../../images/test3.jpg'
+    latestNews: [{
+      "id": 1552623252492,
+      "title": "阿尔斯通高管新书《美国陷阱》揭露 美用司法武器实施全球经济战",
+      "date": "2019-03-14T09:47:37.000Z",
+      "source": "参考消息",
+      "img": "//inews.gtimg.com/newsapp_bt/0/8139088294/641"
+    }, {
+      "id": 1552623252496,
+      "title": "迪士尼收购福克斯，网飞上位，好莱坞“ 新六大” 重 新洗牌",
+      "date": "2019-03-14T04:12:08.000Z",
+      "source": "犀牛娱乐官方",
+      "img": "//inews.gtimg.com/newsapp_bt/0/8133794933/641"
+    }, {
+      "id": 1552623252499,
+      "title": "实现性别平等——来自全球的有影响力女性联大发声",
+      "date": "2019-03-14T04:01:55.000Z",
+      "source": "新华网",
+      "img": "//inews.gtimg.com/newsapp_bt/0/8132916838/641"
+    }, ],
+    newsList: [{
+        "title": "外媒称香港回归15年打破“经济将死”预言",
+        "date": "2012-07-01T09:34:12.000Z",
+        "source": "中国新闻网",
+        "img": "http://img1.gtimg.com/news/pics/hv1/38/85/1076/69988613.jpg"
+      }, {
+        "id": 1519631218591,
+        "title": "实德指媒体窃公司机密 已向某媒体递律师函",
+        "date": "2012-04-21T11:32:04.000Z",
+        "source": "腾讯财经",
+        "img": "http://img1.gtimg.com/finance/pics/hv1/33/207/1023/66573393.jpg"
+      },
+      {
+        "id": 1519631218595,
+        "title": "公务员医疗费用成迷 学者呼吁管理应公开透明",
+        "date": "2012-02-26T08:13:00.000Z",
+        "source": "财新网",
+        "img": "http://img1.gtimg.com/finance/pics/hv1/241/102/983/63945826.jpg"
+      }, {
+        "id": 1552623252481,
+        "title": "王欣会：希望让“来自星星的孩子”有机会过上平等有尊严的生活",
+        "date": "2019-03-15T04:09:11.000Z",
+        "source": "新华网/中国政府网",
+        "img": "//inews.gtimg.com/newsapp_bt/0/8148265330/641"
+      }, {
+        "id": 1552623252486,
+        "title": "支月英：我希望有更多优秀青年投入到乡村教育 助力乡村全面振兴",
+        "date": "2019-03-15T04:08:31.000Z",
+        "source": "新华网/中国政府网",
+        "img": "//inews.gtimg.com/newsapp_bt/0/8148265329/641"
+      }
     ]
 
   },
   onLoad() {}
-    /*API接入
+  /*API接入
     wx.request({
       url: 'https://route.showapi.com/109-35',
       data: {
