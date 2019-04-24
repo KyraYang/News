14d699dbfeea30941aa506c0a835725ece050ce5
Kyra <kyrayang.5@gmail.com>
2019-04-24 12:34:43 +0800

    api set(before go home)

--- pages/index/index.js
+++ pages/index/index.js
@@ -10,29 +10,22 @@ const channelTitleMap = {
   'js': '军事',
 }
 const channelIdMap = {
-
+  //[0]focused, [1]latest
+  'gn': ["5572a108b3cdc86cf39001cd", "5572a109b3cdc86cf39001db"],
+  'gj': ["5572a108b3cdc86cf39001ce", "5572a109b3cdc86cf39001de"],
+  'cj': ["5572a108b3cdc86cf39001d0", "5572a109b3cdc86cf39001e0"],
+  'hlw': ["5572a108b3cdc86cf39001d1", "5572a109b3cdc86cf39001e3"],
+  'ty': ["5572a108b3cdc86cf39001d4", "5572a109b3cdc86cf39001e6"],
+  'kj': ["5572a108b3cdc86cf39001d9", "5572a10ab3cdc86cf39001f4"],
+  'sh': ["5572a109b3cdc86cf39001da", "5572a10bb3cdc86cf39001f8"],
+  'yl': ["5572a108b3cdc86cf39001d5", "5572a10ab3cdc86cf39001eb"],
+  'js': ["5572a108b3cdc86cf39001cf", "5572a109b3cdc86cf39001df"],
 }
+
+
 Page({
   data: {
-    focusedNews: [{
-      "id": 1552623252492,
-      "title": "阿尔斯通高管新书《美国陷阱》揭露 美用司法武器实施全球经济战",
-      "date": "2019-03-14T09:47:37.000Z",
-      "source": "参考消息",
-      "img": "//inews.gtimg.com/newsapp_bt/0/8139088294/641"
-    }, {
-      "id": 1552623252496,
-      "title": "迪士尼收购福克斯，网飞上位，好莱坞“ 新六大” 重 新洗牌",
-      "date": "2019-03-14T04:12:08.000Z",
-      "source": "犀牛娱乐官方",
-      "img": "//inews.gtimg.com/newsapp_bt/0/8133794933/641"
-    }, {
-      "id": 1552623252499,
-      "title": "实现性别平等——来自全球的有影响力女性联大发声",
-      "date": "2019-03-14T04:01:55.000Z",
-      "source": "新华网",
-      "img": "//inews.gtimg.com/newsapp_bt/0/8132916838/641"
-    }],
+    focusedNews: [],
     newsList: [{
         "title": "外媒称香港回归15年打破“经济将死”预言",
         "date": "2012-07-01T09:34:12.000Z",
@@ -75,21 +68,27 @@ Page({
     }
     this.setData({
       channelTitle: channelTitle
-    })}
-    /*API接入
+    })
+    let focusedNews = []
+    for (let x in channelIdMap){
+      console.log(channelIdMap[x][0])
+      this.getNewsContent(channelIdMap[x][0])
+      
+    }
+  },
+
+  //API接入
+  getNewsContent(channelId){
+    console.log(channelId)
     wx.request({
       url: 'https://route.showapi.com/109-35',
       data: {
         "showapi_appid": '92969', //这里需要改成自己的appid
         "showapi_sign": 'eedcb02a3323485a84c33b72074df757', //这里需要改成自己的应用的密钥secret
-        "channelId": "5572a109b3cdc86cf39001db",
-        "channelName": "国内最新",
-        "title": "",
+        "channelId": channelId,
         "page": "1",
-        "needContent": "0",
-        "needHtml": "0",
         "needAllList": "0",
-        "maxResult": "20",
+        "maxResult": "3",
         "id": ""
       },
       header: {
