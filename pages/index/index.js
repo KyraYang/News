394772dd517c9c6d32c4c3114358d67bca15760a
Kyra <kyrayang.5@gmail.com>
2019-04-24 19:02:41 +0800

    onReachBottom set

--- pages/index/index.js
+++ pages/index/index.js
@@ -13,40 +13,27 @@ const channelIdMap = {
   //[0]focused, 
   'gn': "5572a108b3cdc86cf39001cd",
   'gj': "5572a108b3cdc86cf39001ce",
-  'cj': "5572a108b3cdc86cf39001d0", 
-  'hlw': "5572a108b3cdc86cf39001d1", 
-  'ty': "5572a108b3cdc86cf39001d4", 
-  'kj': "5572a108b3cdc86cf39001d9", 
-  'sh': "5572a109b3cdc86cf39001da", 
+  'cj': "5572a108b3cdc86cf39001d0",
+  'hlw': "5572a108b3cdc86cf39001d1",
+  'ty': "5572a108b3cdc86cf39001d4",
+  'kj': "5572a108b3cdc86cf39001d9",
+  'sh': "5572a109b3cdc86cf39001da",
   'yl': "5572a108b3cdc86cf39001d5",
-  'js': "5572a108b3cdc86cf39001cf", 
+  'js': "5572a108b3cdc86cf39001cf",
 }
 
 
 Page({
   data: {
-    focusedNews: {
-      'gn': [],
-      'gj': [],
-      'cj': [],
-      'hlw': [],
-      'ty': [],
-      'kj': [],
-      'sh': [],
-      'yl': [],
-      'js': []},
-    newsList: {
-      'gn': [],
-      'gj': [],
-      'cj': [],
-      'hlw': [],
-      'ty': [],
-      'kj': [],
-      'sh': [],
-      'yl': [],
-      'js': []
+    focusedNews: { 
     },
+    newsList: {
+    },
+    pageNumber: 1,
+    totalPage: 1,
   },
+
+  //开始页面
   onLoad() {
     //set channel bar
     let channelTitle = []
@@ -56,21 +43,65 @@ Page({
     this.setData({
       channelTitle: channelTitle
     })
-   //get gn news
-   this.getNewsContent('gn')
+    //get gn news
+    this.setFirstPage()
   },
 
+
+  //上拉触底出发刷新
+  onReachBottom() {
+    let page = this.data.pageNumber + 1
+    if (page <= this.data.totalPage) {
+      wx.showLoading({
+        title: '加载中...',
+      })
+      wx.request({
+        url: 'https://route.showapi.com/109-35',
+        data: {
+          "showapi_appid": '92969', //这里需要改成自己的appid
+          "showapi_sign": 'eedcb02a3323485a84c33b72074df757', //这里需要改成自己的应用的密钥secret
+          "channelId": channelIdMap.gn,
+          "page": page,
+          "needAllList": "0",
+          "maxResult": 10,
+          "id": ""
+        },
+        header: {
+          'content-type': 'application/json'
+        },
+        success: res => {
+          let newsData = res.data.showapi_res_body.pagebean.contentlist
+          this.data.newsList = this.data.newsList.concat(newsData)
+          console.log(this.data.newsList)
+          console.log(this.data.pageNumber)
+          console.log(this.data.totalPage)
+          this.setData({
+           newsList: this.data.newsList,
+            pageNumber: page,
+          })
+          wx.hideLoading()
+        },
+        fail:res =>{
+          wx.hideLoading()
+        }
+      }, )
+    }
+  },
+
+
+
+
   //API接入
-  getNewsContent(channelId){
+  setFirstPage() {
     wx.request({
       url: 'https://route.showapi.com/109-35',
       data: {
         "showapi_appid": '92969', //这里需要改成自己的appid
         "showapi_sign": 'eedcb02a3323485a84c33b72074df757', //这里需要改成自己的应用的密钥secret
-        "channelId": channelIdMap[channelId],
-        "page": "1",
+        "channelId": channelIdMap.gn,
+        "page": 1,
         "needAllList": "0",
-        "maxResult": "10",
+        "maxResult": 10,
         "id": ""
       },
       header: {
@@ -78,32 +109,29 @@ Page({
       },
       success: res => {
         let newsData = res.data.showapi_res_body.pagebean.contentlist
-        console.log(newsData)
         let imgNews = []
         let listNews = []
-        for (let i=0; i<10; i++){
+        for (let i = 0; i < 10; i++) {
           let oneNews = newsData[i]
-          console.log(oneNews)
-          if (oneNews.havePic && imgNews.length<4){
+          if (oneNews.havePic && imgNews.length < 4) {
             imgNews.push(oneNews)
-          }else{
+          } else {
             listNews.push(oneNews)
           }
         }
-        this.data.focusedNews[channelId] = imgNews
-        this.data.newsList[channelId] = listNews
         this.setData({
-          focusedNews: this.data.focusedNews,
-          newsList: this.data.newsList,
+          focusedNews: imgNews,
+          newsList: listNews,
+          totalPage: res.data.showapi_res_body.pagebean.allPages
         })
-      }
+      },
     })
   },
 
   /*点击轮播图时，跳转到相关新闻
-   onSwiperTap:()=>{
-wx.navigateTo({
-  url: '',
-})
-   }*/
+  onSwiperTap: () => {
+    wx.navigateTo({
+      url: '',
+    })
+  }*/
 })
